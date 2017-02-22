5e3d9a652185f6d5a655f515df436e5b465a608c
Roy Hewitt <roy_hewitt@fws.gov>
2017-02-22 15:34:04 -0500

    add script to inject data during build

---
+++ build/html.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const chalk = require('chalk');
+const cheerio = require('cheerio');
+const moment = require('moment');
+const parallel = require('async/parallel');
+const pug = require('pug');
+const tidy = require('htmltidy').tidy;
+
+const template = pug.compileFile('./src/templates/locations.pug');
+const htmlPath = 'src/index.html';
+const jsonPath = 'src/data/locations.json'
+const htmlOutPath = 'dist/index.html';
+
+parallel([
+  getFile.bind(null, htmlPath),
+  getFile.bind(null, jsonPath)
+], buildPage);
+
+function buildPage (err, results) {
+    if (err) chalk.red(err);
+
+    const $ = cheerio.load(results[0]);
+    const locations = JSON.parse(results[1]);
+    const tidyOptions = {
+      doctype: 'html5',
+      indent: true,
+      outputXml: true,
+      wrap: 10000
+    };
+
+    $('.locations').append( template({ locations }) );
+    $('.last-updated').append(`Last Updated: ${moment().format('MMMM D, YYYY')}`);
+
+    tidy($.html(), tidyOptions, (err, prettyHtml) => {
+      if (err) chalk.red(err);
+      writeOutput(htmlOutPath, prettyHtml);
+    });
+
+  }
+
+function writeOutput(path, data) {
+  fs.writeFile(path, data, err => {
+    if (err) chalk.red(err);
+  });
+}
+
+function getFile (path, cb) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) return cb(err);
+    else return cb(null, data);
+  });
+}
